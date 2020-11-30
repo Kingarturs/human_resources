cda609a28c99701e99f76ba830048ee23586253e
Andres Leal <pixele9@gmail.com>
2020-11-29 23:07:49 -0600

    *added* table structure

--- client/src/screens/IndexPage.js
+++ client/src/screens/IndexPage.js
@@ -16,22 +16,38 @@ function IndexPage() {
     return (
         <>
             <div>
-                <input type="search" placeholder="Search"></input> 
+                <input className="bg-light-blue-500" type="search" placeholder="Search"></input>
                 <button type="button"></button>
             </div>
     
             <div>
-                <table class="table-auto"> 
-                    <thread>
-                        <tr>
-                            <th>Name</th>
-                            <th>Last Name</th>
-                            <th>Phone Number</th>
-                            <th>Email</th>
-                            <th>Adress</th>
-                        </tr>
-                    </thread>
-                </table> 
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th className="w-1/2 px-4 py-2 text-light-blue-600">Name</th>
+                        <th className="w-1/2 px-4 py-2 text-light-blue-600">Last Name</th>
+                        <th className="w-1/2 px-4 py-2 text-light-blue-600">Phone Number</th>
+                        <th className="w-1/2 px-4 py-2 text-light-blue-600">Email</th>
+                        <th className="w-1/2 px-4 py-2 text-light-blue-600">Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="">
+                        <td className="border border-blue-500 px-4 py-2 text-light-blue-600 font-medium">Andres</td>
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">Leal</td>
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">4421234567</td>
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">andres@gmail.com</td>
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">1221 Jackson St.</td>
+                    </tr>
+                    <tr className="bg-blue-200">
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">Ana</td>
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">Diaz</td>
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">4421234567</td>
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">ana@gmail.com</td>
+                        <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">1221 Jackson St.</td>
+                    </tr>
+                </tbody>
+            </table>
             </div>
         </>
     )
