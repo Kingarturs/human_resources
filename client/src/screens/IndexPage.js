51ad6497761199de6c27398817c00b1707934975
Andres Leal <pixele9@gmail.com>
2020-11-30 18:34:04 -0600

    *segmented* table

--- client/src/screens/IndexPage.js
+++ client/src/screens/IndexPage.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 import SearchBar from '../components/SearchBar';
+import Table from '../components/Table';
 
 import './IndexPage.css'
 
@@ -41,51 +42,9 @@ function IndexPage() {
 
     return (
         <>
-            {/* <div className="flex items-center justify-center w-full h-1/6 bg-blue-200">
-                <input className="w-3/4 h-10 px-8 py-6 rounded-xl outline-none shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" type="search" placeholder="Search" id="search-bar"></input>
-                <button onClick={() => {
-                    const searchValue = document.querySelector("#search-bar").value
-                    console.log("value ", searchValue)
-                    if(searchValue !== "") {
-                        history.push(`/search/${searchValue}`)
-                    }
-                }} className="bg-blue-600 rounded-xl w-12 h-12 ml-3 shadow-xl transform transition hover:scale-90 focus:outline-none" type="button"><i className="text-white fas fa-search"></i></button>
-            </div> */}
             <SearchBar />
-
             <div className="p-24">
-            <table className="table-auto p-24">
-                <thead>
-                    <tr>
-                        <th className="w-1/6 px-4 py-2 text-blue-600">Name</th>
-                        <th className="w-1/6 px-4 py-2 text-blue-600">Last Name</th>
-                        <th className="w-1/6 px-4 py-2 text-blue-600">Phone Number</th>
-                        <th className="w-1/6 px-4 py-2 text-blue-600">Email</th>
-                        <th className="w-1/6 px-4 py-2 text-blue-600">Address</th>
-                        <th className="w-1/6 px-4 py-2 text-blue-600">Edit</th>
-                        <th className="w-1/6 px-4 py-2 text-blue-600">Remove</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        employees.map(employee => {
-                            const {name, lastname, phone, email, address} = employee
-
-                            return(
-                                <tr className="">
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{name}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{lastname}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{phone}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{email}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{address}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium text-center"><a href="/edit"><i class="fas fa-user-edit"></i></a></td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium text-center"><a href="/delete"><i class="fas fa-minus-circle"></i></a></td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
+            <Table employees={employees} /> 
             <a href="/newEmployee">
                 <button className="w-16 h-16 absolute bottom-12 right-12 transform transition hover:scale-90 hover:shadow-lg focus:outline-none rounded-full bg-blue-600 shadow-xl text-white">
                     <i className="fas fa-plus"></i>
--- client/src/screens/SearchEmployee.js
+++ client/src/screens/SearchEmployee.js
@@ -4,6 +4,7 @@ import { useHistory, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 import SearchBar from '../components/SearchBar';
+import Table from '../components/Table';
 
 import './IndexPage.css'
 
@@ -35,45 +36,10 @@ function IndexPage() {
 
     return (
         <>
-            {/* <div className="flex items-center justify-center w-full h-1/6 bg-blue-200">
-                <input className="w-3/4 h-10 px-8 py-6 rounded-xl outline-none shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" type="search" placeholder="Search"></input>
-                <button className="bg-blue-600 rounded-xl w-12 h-12 ml-3 shadow-xl transform transition hover:scale-90 focus:outline-none" type="button"><i className="text-white fas fa-search"></i></button>
-            </div> */}
 			<SearchBar />
     
             <div className="p-24">
-            <table className="table-auto p-24">
-                <thead>
-                    <tr>
-                        <th className="w-1/2 px-4 py-2 text-blue-600">Name</th>
-                        <th className="w-1/2 px-4 py-2 text-blue-600">Last Name</th>
-                        <th className="w-1/2 px-4 py-2 text-blue-600">Phone Number</th>
-                        <th className="w-1/2 px-4 py-2 text-blue-600">Email</th>
-                        <th className="w-1/2 px-4 py-2 text-blue-600">Address</th>
-                        <th className="w-1/2 px-4 py-2 text-blue-600">Edit</th>
-                        <th className="w-1/2 px-4 py-2 text-blue-600">Remove</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        results.map(employee => {
-                            const {name, lastname, phone, email, address} = employee
-
-                            return(
-                                <tr className="">
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{name}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{lastname}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{phone}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{email}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{address}</td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium"><a href="/edit"><i class="fas fa-user-edit"></i></a></td>
-                                    <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium"><a href="/delete"><i class="fas fa-minus-circle"></i></a></td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
+			<Table employees={results} /> 
 			<div className="flex items-center justify-content w-full mt-5">
 				<a href="/" className="text-center w-full text-blue-600">Display all employees</a>
 			</div>
